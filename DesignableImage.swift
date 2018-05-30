979abeeee372829340b790ac885e6c5f28bc401e
Ketan patel <ketan@mobappbuddy.com>
2018-05-30 10:57:32 +0530

    Add files via upload

---
+++ DesignableImage.swift
@@ -0,0 +1,38 @@
+//
+//  Designable.swift
+//  ibdesignable
+//
+//  Created by iCoderzBinal on 6/8/17.
+//  Copyright © 2017 iCoderzSameer. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class DesignableImage: UIImageView {
+
+    
+    @IBInspectable var borderWidth: CGFloat = 0.0{
+        didSet{
+            self.layer.borderWidth = borderWidth;
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor = UIColor.clear{
+        didSet{
+            self.layer.borderColor = borderColor.cgColor;
+        }
+    }
+    @IBInspectable var cornerradious: CGFloat = 0{
+        didSet{
+            self.layer.cornerRadius = cornerradious;
+        }
+    }
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
